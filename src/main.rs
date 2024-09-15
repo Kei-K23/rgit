78622de75b739ba0049d5da63d17c01d5226ceec
Kei-K23 <keiksl2301@gmail.com>
2024-09-15 18:48:40 +0630

    Fefactor the config function and file structure

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod add;
 mod branch;
 mod checkout;
 mod commit;
+mod config;
 mod diff;
 mod helper;
 mod init;
@@ -13,126 +14,16 @@ use branch::{branch, delete_branch};
 use checkout::checkout;
 use clap::{Arg, Command};
 use commit::commit;
+use config::handle_config_command;
 use diff::diff;
 use init::init;
 use log::log;
 use status::status;
 use std::{
-    fs::{self, File, OpenOptions},
-    io::{self, BufRead, BufReader, Write},
+    fs::{self, File},
+    io::{self, Write},
     path::Path,
 };
-
-// Configuration command
-fn set_config(key: &str, value: &str) -> io::Result<()> {
-    let config_file_path = ".rgit/config"; // config file path
-
-    // Read config file if it exist and get value and store them in lines String Vector
-    let mut lines: Vec<String> = vec![];
-    if let Ok(config_file) = File::open(config_file_path) {
-        let config_file_rdr = BufReader::new(config_file);
-        for line in config_file_rdr.lines() {
-            lines.push(line?);
-        }
-    }
-
-    // Find or add '[user]' section and if already have user section then update the value according to key
-    let mut is_user_section = false;
-    let mut updated = false;
-
-    // Loop through lines of config file
-    for line in lines.iter_mut() {
-        if line.trim() == "[user]" {
-            is_user_section = true;
-        } else if is_user_section && line.trim().starts_with(key) {
-            *line = format!("    {} = {}", key, value); // Update the value when match with key
-            updated = true;
-            break;
-        } else if line.trim().is_empty() {
-            is_user_section = false;
-        }
-    }
-
-    // Handle case for new config file or config that does not have '[user]' section
-    if !updated {
-        if !is_user_section {
-            lines.push("[user]".to_string());
-        }
-        // Add new config for user
-        lines.push(format!("    {} = {}", key, value));
-    }
-
-    // Write new update config file back to .rgit/config file
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open(config_file_path)?;
-
-    for line in lines {
-        writeln!(file, "{}", line)?;
-    }
-
-    println!("Configuration updated: {}={}", key, value);
-
-    Ok(())
-}
-
-fn get_config(key: &str) -> io::Result<Option<String>> {
-    let config_file_path = ".rgit/config";
-
-    if let Ok(config_file) = File::open(config_file_path) {
-        // Create buffer to read line by line to config file content
-        let config_file_rdr = BufReader::new(config_file);
-        let mut in_user_section = true; // Indicator for '[user]' section
-
-        for line in config_file_rdr.lines() {
-            let line = line?;
-            if line.trim() == "[user]" {
-                in_user_section = true // By defining in_user_section indicator to true make sure below content will be associated with '[user]' section
-            } else if in_user_section {
-                // Split string by " = "
-                if let Some((config_key, config_value)) = line.trim().split_once(" = ") {
-                    // Match with user provided key
-                    if config_key == key {
-                        return Ok(Some(config_value.to_string()));
-                    }
-                }
-            } else if line.trim().is_empty() {
-                in_user_section = false;
-            }
-        }
-    }
-
-    println!("No configuration found for '{}'", key);
-    Ok(None)
-}
-
-// Handle config commands (set or get)
-fn handle_config_command(action: &str, key: &str, value: Option<&str>) -> io::Result<()> {
-    match action {
-        "set" => {
-            if let Some(val) = value {
-                set_config(key, val)?;
-                println!("Configuration set: {} = {}", key, val);
-            } else {
-                println!("Value required for 'set' command.");
-            }
-        }
-        "get" => {
-            if let Some(val) = get_config(key)? {
-                println!("{} = {}", key, val);
-            } else {
-                println!("No configuration found for '{}'", key);
-            }
-        }
-        _ => {
-            println!("Invalid action. Use 'set' or 'get'.");
-        }
-    }
-
-    Ok(())
-}
 
 fn tag(tag_name: &str) -> io::Result<()> {
     // TODO: Change heard code head_file value (currently master) to dynamic according to active branch
